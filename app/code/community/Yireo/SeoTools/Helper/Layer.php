e938751e3f0c1493e802c91d07c7bb219c8b7bde
Jisse Reitsma <info@yireo.com>
2013-08-18 11:44:48 +0200

    Basic helper for layered navigation

---
+++ app/code/community/Yireo/SeoTools/Helper/Layer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Yireo SeoTools for Magento 
+ *
+ * @package     Yireo_SeoTools
+ * @author      Yireo (http://www.yireo.com/)
+ * @copyright   Copyright (c) 2013 Yireo (http://www.yireo.com/)
+ * @license     Open Source License
+ */
+
+/**
+ * SeoTools helper for layered navigation
+ */
+class Yireo_SeoTools_Helper_Layer extends Mage_Core_Helper_Abstract
+{
+    /*
+     * Method to get the current layered navigation filters
+     *
+     * @access public
+     * @param null
+     * @return array
+     */
+    public function getFilters()
+    {
+        $filters = Mage::getSingleton('catalog/layer')->getState()->getFilters();
+        return $filters;
+    }    
+}
