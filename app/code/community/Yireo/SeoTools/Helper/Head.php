828b29d33d24271fb86fe7f8009d7581936d5ccc
Jisse Reitsma <info@yireo.com>
2013-08-18 11:45:00 +0200

    Basic helper for head-manipulation

---
+++ app/code/community/Yireo/SeoTools/Helper/Head.php
@@ -0,0 +1,168 @@
+<?php
+/**
+ * Yireo SeoTools for Magento 
+ *
+ * @package     Yireo_SeoTools
+ * @author      Yireo (http://www.yireo.com/)
+ * @copyright   Copyright (c) 2013 Yireo (http://www.yireo.com/)
+ * @license     Open Source License
+ */
+
+/**
+ * SeoTools helper to modify the HTML-head
+ */
+class Yireo_SeoTools_Help_Data extends Mage_Core_Helper_Abstract
+{
+    /*
+     * Method to get the document title
+     *
+     * @access public
+     * @param null
+     * @return string
+     */
+    public function getTitle()
+    {
+        // Get the head-block and fetch the title
+        $headBlock = $this->getLayout()->getBlock('head');
+        if ($headBlock) {
+            return $headBlock->getTitle();
+        }
+    }
+
+    /*
+     * Method to set the document title
+     *
+     * @access public
+     * @param string $title New document title to set
+     * @return bool
+     */
+    public function setTitle($title = null)
+    {
+        // Get the head-block and set the title
+        $headBlock = $this->getLayout()->getBlock('head');
+        if ($headBlock) {
+            $headBlock->setTitle($title);
+            return true;
+        }
+        return false;
+    }
+
+    /*
+     * Method to get the document keywords
+     *
+     * @access public
+     * @param null
+     * @return array
+     */
+    public function getKeywords()
+    {
+        // Get the head-block and get the keywords
+        $headBlock = $this->getLayout()->getBlock('head');
+        if ($headBlock) {
+            $keywords = $headBlock->getKeywords();
+
+            // Convert the keyword-string into a clean array
+            $keywords = implode(',', $keywords);
+            foreach($keywords as $keywordIndex => $keyword) {
+                $keywords[$keywordIndex] = trim($keyword);
+            }
+
+            return $keywords;
+        }
+        return array();
+    }
+
+    /*
+     * Method to set the document keywords
+     *
+     * @access public
+     * @param $keywords array List of keywords to insert
+     * @param $prepend false Append or prepend the keywords
+     * @param $overwrite false Overwrite current list of keywords
+     * @return bool
+     */
+    public function setKeywords($keywords = array(), $prepend = false, $overwrite = false)
+    {
+        // Get the head-bloc
+        $headBlock = $this->getLayout()->getBlock('head');
+        if (empty($headBlock)) {
+            return false;
+        }
+
+        // Append or prepend the keywords
+        if($overwrite == false) {
+            $currentKeywords = $this->getKeywords();
+            if($prepend == true) {
+                $keywords = array_merge($keywords, $currentKeywords);
+            } else {
+                $keywords = array_merge($currentKeywords, $keywords);
+            }
+        }
+
+        // Correct the array
+        if(is_array($keywords)) {
+            $keywords = array_unique($keywords);
+            $keywords = implode(', ', $keywords);
+        }
+
+        // Actually set the keywords as a string 
+        $headBlock->setKeywords($keywords);
+        return true;
+    }
+
+    /*
+     * Method to get the current store name
+     *
+     * @access public
+     * @param null
+     * @return string
+     */
+    public function getStoreName()
+    {
+        return Mage::getStoreConfig('system/store/name');
+    }
+
+    /*
+     * Method to get the seperator
+     *
+     * @access public
+     * @param null
+     * @return string
+     */
+    public function getSeperator()
+    {
+        return ' '.Mage::getStoreConfig('catalog/seo/title_separator').' ';
+    }
+
+    /*
+     * Method to get the current category-title
+     *
+     * @access public
+     * @param null
+     * @return string
+     */
+    public function getCategoryTitle()
+    {
+        $category = Mage::registry('current_category');
+        if(!empty($category)) {
+            return $category->getName();
+        }
+        return null;
+    }
+
+    /*
+     * Method to get the current product-title
+     *
+     * @access public
+     * @param null
+     * @return string
+     */
+    public function getProductTitle()
+    {
+        $product = Mage::registry('current_product');
+        if(!empty($product)) {
+            return $product->getName();
+        }
+        return null;
+    }
+}
